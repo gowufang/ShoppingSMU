5a462c0962af4cee41487ea42ae8163cb655aa58
Volvane <gowufang@gmail.com>
2017-12-26 23:50:48 +0800

    test Stetho to see database on Chrome

--- example/src/main/java/me/wufang/shoppingsmu/ExampleApp.java
+++ example/src/main/java/me/wufang/shoppingsmu/ExampleApp.java
@@ -2,6 +2,7 @@ package me.wufang.shoppingsmu;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
 import com.joanzapata.iconify.fonts.FontAwesomeModule;
 
 import me.wufang.volvane.app.Volvane;
@@ -25,6 +26,15 @@ public class ExampleApp extends Application {
                 .withApiHost("http://127.0.0.1")
                 .configure();
 
+        initStetho();
         DatabaseManager.getInstance().init(this);
+    }
+
+        private void initStetho() {
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+                        .build());
     }
 }
