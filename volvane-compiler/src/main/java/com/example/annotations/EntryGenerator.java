fa7f7883261fb6cb43064f113d8770bd8ec6412b
Volvane <gowufang@gmail.com>
2018-01-02 21:28:29 +0800

    finish EntryGenerator

--- volvane-compiler/src/main/java/com/example/annotations/EntryGenerator.java
+++ volvane-compiler/src/main/java/com/example/annotations/EntryGenerator.java
@@ -1,9 +1,17 @@
 package com.example.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by wu on 2018/1/2.
  * Email:gowufang@gmail.com
  */
-
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
 public @interface EntryGenerator {
+    String packageName();
+    Class<?> entryTemplate();
 }
